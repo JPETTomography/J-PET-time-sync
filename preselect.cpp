b34d3c4ee4994c9fb693fab1392b1b8d38c58918
rundel <alexrndl@gmail.com>
2016-07-18 14:12:06 +0200

    preselection fix

--- preselect.cpp
+++ preselect.cpp
@@ -12,10 +12,10 @@ int main(int argc, char* argv[]) {
 	frameworkargs[0]=argv[0];
 	frameworkargs[1]="-l";
 	frameworkargs[2]="large_barrel.json";
-	frameworkargs[argc]="-i";
-	frameworkargs[argc+1]="43";
-	for(size_t i=1;i<argc;i++)frameworkargs[i+2]=argv[i];
-	JPetManager::getManager().parseCmdLine(argc+2, frameworkargs);
+	frameworkargs[3]="-i";
+	frameworkargs[4]="43";
+	for(size_t i=1;i<argc;i++)frameworkargs[i+4]=argv[i];
+	JPetManager::getManager().parseCmdLine(argc+4, frameworkargs);
 	JPetManager::getManager().registerTask([](){
 		return new JPetTaskLoader("hld", "tslot.raw", new TaskA("Module A: Unp to TSlot Raw","Process unpacked HLD file into a tree of JPetTSlot objects"));
 	});
