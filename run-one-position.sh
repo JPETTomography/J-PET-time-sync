eece22e1ab436ea77e0e488c912252cab0b7a3e2
alexkernphysiker <kernphysiker@openmailbox.org>
2016-07-29 12:42:26 +0200

    script update

--- run-one-position.sh
+++ run-one-position.sh
@@ -9,7 +9,7 @@ done
 echo ${paramlist}
 ./SyncAB-fits 4${paramlist} > AB-center.txt
 for X in `find $1|grep .root|grep .phys.hit.means.`; do
-	./SyncStrips-preselect -t hld -f ${X} < AB-center.txt
+	./SyncStrips-preselect -t root -f ${X} < AB-center.txt
 done
 paramlist=""
 for X in `find $1|grep .root|grep .Stat4Strips.`; do
