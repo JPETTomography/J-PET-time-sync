0539b1dfa4627ac9c6e215d34035f07c93d986d1
alexkernphysiker <kernphysiker@openmailbox.org>
2016-09-12 11:10:24 +0200

    preselection script update

--- analyse-prepare.sh
+++ analyse-prepare.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 for X in `find $1|grep .hld`; do
 	echo "Preparing file  ${X}"
-	./Prepare-preselect -t hld -f ${X}
+	if [-e ${X%.*}.phys.hit.means.root]; then
+		echo "has been analysed"
+	else
+		echo "starting analysis"
+		./Prepare-preselect -t hld -f ${X}
+	fi
 done
