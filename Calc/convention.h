982a9d689c7c2987936966a46d04eeb7793de0bf
alexkernphysiker <kernphysiker@openmailbox.org>
2016-09-16 15:00:39 +0200

    using upgraded version of << and >> operators for value<>

--- Calc/convention.h
+++ Calc/convention.h
@@ -7,47 +7,27 @@ struct SyncAB_results{
   inline const MathTemplates::value<double> Range()const{return {position.val(),1.0*width.val()};}
 };
 inline std::istream&operator>>(std::istream&str,SyncAB_results&item){
-  double v,u;
-  str>>v>>u;item.position={v,u};
-  str>>v>>u;item.width={v,u};
-  str>>item.chi_sq;
-  return str;
+  return str>>item.position>>item.width>>item.chi_sq;
 }
 inline std::ostream&operator<<(std::ostream&str,const SyncAB_results&item){
-  str<<item.position.val()<<"\t"<<item.position.uncertainty()<<"\t"<<item.width.val()<<"\t"<<item.width.uncertainty()<<"\t"<<item.chi_sq;
-  return str;
+  return str<<item.position<<"\t"<<item.width<<"\t"<<item.chi_sq;
 }
 struct SyncOposite_results{
   MathTemplates::value<double>position,width;double chi_sq;
 };
 inline std::istream&operator>>(std::istream&str,SyncOposite_results&item){
-  double v,u;
-  str>>v>>u;item.position={v,u};
-  str>>v>>u;item.width={v,u};
-  str>>item.chi_sq;
-  return str;
+  return str>>item.position>>item.width>>item.chi_sq;
 }
 inline std::ostream&operator<<(std::ostream&str,const SyncOposite_results&item){
-  str<<item.position.val()<<"\t"<<item.position.uncertainty()<<"\t"<<item.width.val()<<"\t"<<item.width.uncertainty()<<"\t"<<item.chi_sq;
-  return str;
+  return str<<item.position<<"\t"<<item.width<<"\t"<<item.chi_sq;
 }
 struct SyncNeighbour_results{
   MathTemplates::value<double>position_left,width_left,position_right,width_right;double chi_sq;
 };
 inline std::istream&operator>>(std::istream&str,SyncNeighbour_results&item){
-  double v,u;
-  str>>v>>u;item.position_left={v,u};
-  str>>v>>u;item.width_left={v,u};
-  str>>v>>u;item.position_right={v,u};
-  str>>v>>u;item.width_right={v,u};
-  str>>item.chi_sq;
-  return str;
+  return str>>item.position_left>>item.width_left>>item.position_right>>item.width_right>>item.chi_sq;
 }
 inline std::ostream&operator<<(std::ostream&str,const SyncNeighbour_results&item){
-  str
-  <<item.position_left.val()<<"\t"<<item.position_left.uncertainty()<<"\t"<<item.width_left.val()<<"\t"<<item.width_left.uncertainty()
-  <<item.position_right.val()<<"\t"<<item.position_right.uncertainty()<<"\t"<<item.width_right.val()<<"\t"<<item.width_right.uncertainty()
-  <<"\t"<<item.chi_sq;
-  return str;
+  return str<<item.position_left<<"\t"<<item.width_left<<"\t"<<item.position_right<<"\t"<<item.width_right<<"\t"<<item.chi_sq;
 }
 #endif
