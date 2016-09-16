9aaf97bb6c0084fe9a9437eac4c4039184435fd9
alexkernphysiker <kernphysiker@openmailbox.org>
2016-09-16 16:06:30 +0200

    new analysis stage added. stage 1

--- analyse-sync.sh
+++ analyse-sync.sh
@@ -22,3 +22,5 @@ done
 echo ${paramlist}
 ./SyncStrips-oposite $1${paramlist} > ${2}/Oposite-strips.txt
 ./SyncStrips-neighbour $1${paramlist} > ${2}/Neighbour-strips.txt
+
+Synchro-solve-equations $1 ${2}/AB-center.txt ${2}/Oposite-strips.txt ${2}/Neighbour-strips.txt > Deltas.txt
