07aac49f6fdf27998bb3f8d3b31dee796e2a8057
alexkernphysiker <kernphysiker@openmailbox.org>
2016-09-21 13:50:49 +0200

    scripts fix

--- analyse-sync.sh
+++ analyse-sync.sh
@@ -23,4 +23,4 @@ echo ${paramlist}
 ./SyncStrips-oposite $1${paramlist} > ${2}/Oposite-strips.txt
 ./SyncStrips-neighbour $1${paramlist} > ${2}/Neighbour-strips.txt
 
-Synchro-solve-equations $1 ${2}/AB-center.txt ${2}/Oposite-strips.txt ${2}/Neighbour-strips.txt > Deltas.txt
+./Synchro-solve-equations $1 ${2}/AB-center.txt ${2}/Oposite-strips.txt ${2}/Neighbour-strips.txt > Deltas.txt
