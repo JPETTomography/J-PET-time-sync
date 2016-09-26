a11a8c553089c9e72db35a7ba8df43a0375b5ff9
alexkernphysiker <kernphysiker@openmailbox.org>
2016-09-26 17:14:15 +0200

    genetic alg. conditions fixed

--- Calc/SyncAB.cpp
+++ Calc/SyncAB.cpp
@@ -38,13 +38,13 @@ namespace Sync{
 	fit.Init(25*TotalFunc::ParamCount,make_shared<GenerateByGauss>()
 	    <<make_pair(total,total*30.0)
 	    <<make_pair(hist.left().X().min()+hist.right().X().max()/2.0,hist.right().X().max()-hist.left().X().min())
-	    <<make_pair(0.1,0.5)
+	    <<make_pair(1.0,10.0)
 	    <<make_pair(hist.left().X().max(),hist.right().X().max()-hist.left().X().min())
 	    <<make_pair(-0.5,0.5)
 	    <<make_pair(hist.right().X().min(),hist.right().X().max()-hist.left().X().min())
 	    <<make_pair(0.5,0.5)
 	    <<make_pair(total,total*20.0)
-	    <<make_pair(2.0,20.0)
+	    <<make_pair(0.0,20.0)
 	,r);
 	cerr<<fit.ParamCount()<<" parameters"<<endl;
 	cerr<<fit.PopulationSize()<<" points"<<endl;
