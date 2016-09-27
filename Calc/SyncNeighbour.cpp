8a49b1124c59e517bd897e66a5dd64adc00fe69f
alexkernphysiker <kernphysiker@openmailbox.org>
2016-09-27 15:58:15 +0200

    Nieghbour synchronization. assymetry estimation

--- Calc/SyncNeighbour.cpp
+++ Calc/SyncNeighbour.cpp
@@ -24,7 +24,7 @@ namespace Sync{
 	    return (P[0]>0)&&(P[2]>0)&&(P[3]>0)&&(P[5]>0)
 	    &&(P[1]>hist.left().X().max())&&(P[4]<hist.right().X().min())
 	    &&((P[4]-P[1])>((P[2]+P[5])/2.0))
-	    &&((P[4]-P[1])<((P[2]+P[5])*7.0))
+	    &&((P[4]-P[1])<((P[2]+P[5])*10.0))
 	    &&((P[2]/P[5])<2.0)&&((P[5]/P[2])<2.0)
 	    &&((P[0]/P[3])<5.0)&&((P[3]/P[0])<5.0);
 	});
@@ -33,10 +33,10 @@ namespace Sync{
 	fit.Init(500,make_shared<GenerateByGauss>()
 	    <<make_pair(total,total*30.0)
 	    <<make_pair((hist.left().X().min()+hist.right().X().max())/2.0,(hist.right().X().max()-hist.left().X().min())/2.0)
-	    <<make_pair(1.5,2.5)
+	    <<make_pair(1.0,1.5)
 	    <<make_pair(total,total*30.0)
 	    <<make_pair((hist.left().X().min()+hist.right().X().max())/2.0,(hist.right().X().max()-hist.left().X().min())/2.0)
-	    <<make_pair(1.5,2.5)
+	    <<make_pair(1.0,1.5)
 	,r);
 	cerr<<fit.ParamCount()<<" parameters"<<endl;
 	cerr<<fit.PopulationSize()<<" points"<<endl;
@@ -54,11 +54,11 @@ namespace Sync{
 	cerr<<endl<<"done. chi^2/D="<<chi_sq_norm<<endl;
 	fit.SetUncertaintyCalcDeltas(parEq(fit.ParamCount(),0.01));
 	for(const auto&P:fit.ParametersWithUncertainties())cerr<<P<<endl;
-	auto F=[](double x){return log(x);};
+	auto asym_log=[](double x){return log2(x);};
 	return {
 	    .position_left=fit.ParametersWithUncertainties()[1],.width_left=fit.ParametersWithUncertainties()[2],
 	    .position_right=fit.ParametersWithUncertainties()[4],.width_right=fit.ParametersWithUncertainties()[5],
-	    .assymetry=func_value<double>(F, fit.ParametersWithUncertainties()[3]/fit.ParametersWithUncertainties()[0] ),
+	    .assymetry=func_value<double>(asym_log, fit.ParametersWithUncertainties()[3]/fit.ParametersWithUncertainties()[0] ),
 	    .chi_sq=chi_sq_norm
 	};
 	
