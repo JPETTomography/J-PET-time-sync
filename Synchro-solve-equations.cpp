f7d9d51424508874627b1d468bd1e50942347be9
alexkernphysiker <kernphysiker@openmailbox.org>
2016-09-29 14:51:23 +0200

    simplification of the system of equation. stage 1

--- Synchro-solve-equations.cpp
+++ Synchro-solve-equations.cpp
@@ -41,58 +41,51 @@ int main(int argc, char **argv) {
     auto DeltaT=make_JPetMap<DeltaT_results>();
     Plotter::Instance().SetOutput(".","delta_t_sync");
     for(size_t layer=1;layer<=DeltaT->LayersCount();layer++){
-	hist<double> A,B,eq_hist_left,eq_hist_right;
-	cerr<<"LAYER "<<layer<<" : "<<endl;
+	cerr<<"=======LAYER "<<layer<<" : "<<endl;
 	list<InexactEquation> equations;
 	const size_t N=DeltaT->LayerSize(layer);
 	for(size_t i=1;i<=N;i++){
-	    const auto&AB_sync=AB->Item(layer,i);
-	    const size_t A=i-1,B=A+N;
-	    equations.push_back(in_eq( [A,B](const ParamSet&delta){return delta[B]-delta[A];} , {AB_sync.position.val(),AB_sync.width.val()} ));
 	    const auto&neighbour_sync=Nei->Item(layer,i);
-	    const size_t An=(A+3)%N,Bn=An+N;
 	    value<double> leftpeak={neighbour_sync.position_left.val(),neighbour_sync.width_left.val()},
 		rightpeak={neighbour_sync.position_right.val(),neighbour_sync.width_right.val()};
-	    equations.push_back(in_eq([A,B,An,Bn](const ParamSet&delta){return (delta[Bn]+delta[An])-(delta[B]+delta[A]);}, leftpeak+rightpeak ));
+	    equations.push_back(in_eq([i,N](const ParamSet&delta){return delta[(i+3)%N]-delta[i];}, (leftpeak+rightpeak)/2.0 ));
 	}
 	for(size_t i=1;i<=(N/2);i++){
 	    const auto&opo_sync=Opo->Item(layer,i);
-	    const size_t A=i-1,B=A+N;
-	    const size_t Ao=A+(N/2),Bo=Ao+N;
-	    equations.push_back(in_eq([A,B,Ao,Bo](const ParamSet&delta){return ((delta[Bo]+delta[Ao])-(delta[B]+delta[A]))/2.0;}, {opo_sync.position.val(),opo_sync.width.val()}));
+	    equations.push_back(in_eq([i,N](const ParamSet&delta){return delta[i+(N/2)]-delta[i];}, {opo_sync.position.val(),opo_sync.width.val()}));
 	}
 	cerr<<equations.size()<<" equations"<<endl;
-	InexactEquationSolver<DifferentialMutations<>> solver(equations);
+	cerr<<"hits:"<<endl;
+	InexactEquationSolver<DifferentialMutations<>> solver_hits(equations);
 	auto init=make_shared<GenerateByGauss>();
-	while(init->Count()<(N*2))init<<make_pair(0,30);
-	solver.Init(N*5,init,engine);
-	cerr<<solver.ParamCount()<<" variables"<<endl;
-	cerr<<solver.PopulationSize()<<" points"<<endl;
-	while(!solver.AbsoluteOptimalityExitCondition(0.000001)){
-	    solver.Iterate(engine);
-	    cerr<<solver.iteration_count()<<" iterations; "
-	    <<solver.Optimality()<<"<chi^2<"
-	    <<solver.Optimality(solver.PopulationSize()-1)
+	while(init->Count()<N)init<<make_pair(0,50);
+	solver_hits.Init(N*20,init,engine);
+	cerr<<solver_hits.ParamCount()<<" variables"<<endl;
+	cerr<<solver_hits.PopulationSize()<<" points"<<endl;
+	while(!solver_hits.AbsoluteOptimalityExitCondition(0.000001)){
+	    solver_hits.Iterate(engine);
+	    cerr<<solver_hits.iteration_count()<<" iterations; "
+	    <<solver_hits.Optimality()<<"<chi^2<"
+	    <<solver_hits.Optimality(solver_hits.PopulationSize()-1)
 	    <<"                 \r";
 	}
 	cout<<endl;
-	cout<<"chi^2/D = "<<solver.Optimality()/(equations.size()-solver.ParamCount())<<endl;
-	solver.SetUncertaintyCalcDeltas(parEq(solver.ParamCount(),0.01));
-	for(size_t i=1;i<=N;i++){
-	    A<<point<value<double>>(double(i),solver.ParametersWithUncertainties()[i-1]);
-	    B<<point<value<double>>(double(i),solver.ParametersWithUncertainties()[i-1+N]);
-	    DeltaT->Item(layer,i).A=solver.ParametersWithUncertainties()[i-1];
-	    DeltaT->Item(layer,i).B=solver.ParametersWithUncertainties()[i-1+N];
+	cout<<"chi^2/D = "<<solver_hits.Optimality()/(equations.size()-solver_hits.ParamCount())<<endl;
+	solver_hits.SetUncertaintyCalcDeltas(parEq(solver_hits.ParamCount(),0.01));
+	hist<double> eq_left,eq_right,delta_hits;
+	for(size_t i=0;i<N;i++)
+	    delta_hits<<point<value<double>>(double(i+1),solver_hits.ParametersWithUncertainties()[i]);
+	for(const auto&eq:equations){
+	    static size_t i=0;
+	    eq_left<<point<value<double>>(double(i),eq.first(solver_hits.Parameters()));
+	    eq_right<<point<value<double>>(double(i),eq.second);
+	    i++;
 	}
-	{size_t index=0;
-	    for(const auto&eq:equations){
-		eq_hist_left<<point<value<double>>(double(index),eq.first(solver.Parameters()));
-		eq_hist_right<<point<value<double>>(double(index),eq.second);
-		index++;
-	    }
-	}
-	Plot<double>().Hist(A,"A").Hist(B,"B")<<"set xlabel 'strip number'"<<"set ylabel 'time offset'"<<"set key on";
-	Plot<double>().Hist(eq_hist_left,"left").Hist(eq_hist_right,"right")<<"set xlabel 'equation index'"<<"set ylabel 'value'"<<"set key on";
+	Plot<double>().Hist(eq_left,"left").Hist(eq_right,"right")<<"set key on"<<"set xlabel 'layer "+to_string(layer)+"'";
+	Plot<double>().Hist(delta_hits,"HITS")<<"set key on"<<"set xlabel 'layer "+to_string(layer)+"'";
+	//ToDo: SyncAB
+	
+	
     }
     cout<<(*DeltaT);
 }
