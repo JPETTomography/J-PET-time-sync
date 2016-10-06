ed0e9379f5e6baea59d174b5e910bea5667c82ad
alexkernphysiker <kernphysiker@openmailbox.org>
2016-10-06 13:57:25 +0200

    separating of two synchronizations

---
+++ AppSyncWithRef/Preselect.cpp
@@ -0,0 +1,23 @@
+#include <JPetManager/JPetManager.h>
+#include <JPetTaskLoader/JPetTaskLoader.h>
+#include <Preselection/PreselectionMain.h>
+#include <Preselection/TaskA.h>
+#include <Preselection/TaskB.h>
+#include <Preselection/TaskC.h>
+#include <Preselection/TaskD.h>
+#include <Preselection/SyncAB.h>
+using namespace std;
+void AddModules() {
+	JPetManager::getManager().registerTask([](){
+		return new JPetTaskLoader("hld", "tslot.raw", new TaskA("Module A: Unp to TSlot Raw","Process unpacked HLD file into a tree of JPetTSlot objects"));
+	});
+	JPetManager::getManager().registerTask([](){
+		return new JPetTaskLoader("tslot.raw", "raw.sig",new TaskB("Module B: TSlot Cal to Raw Signal","Build Raw Signals from Calibrated TSlots"));
+	});
+	JPetManager::getManager().registerTask([](){
+		return new JPetTaskLoader("raw.sig", "phys.hit",new TaskC("Module C: Pair signals","Create hits from pairs of signals")); 
+	}); 
+	JPetManager::getManager().registerTask([](){
+		return new JPetTaskLoader("phys.hit", "phys.hit.means",new TaskD("Module D: Make histograms for hits","Only make timeDiff histos and produce mean timeDiff value for each threshold and slot to be used by the next module"));
+	});
+}
