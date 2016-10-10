73f398335b8166e80267bd33ea90339ae8d3925c
alexkernphysiker <kernphysiker@openmailbox.org>
2016-10-10 16:06:19 +0200

    script fix

--- analyse2-tof.sh
+++ analyse2-tof.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-filelist=`find $2|grep .root|grep .phys.hit.`
+filelist=`find $1|grep .root|grep .phys.hit.`
 echo ${filelist}
 for X in ${filelist}; do
 	echo "TOF build : ${X}"
 	./TOF-build -t root -f ${X}
 done
 paramlist=""
-for X in `find $2|grep .root|grep .TOF.stat.`; do
+for X in `find $1|grep .root|grep .TOF.stat.`; do
 	paramlist=${paramlist}" "${X}
 done
 echo ${paramlist}
-./TOF-hists-before $1${paramlist} > ${2}/TOF-cut.txt
+./TOF-hists-before${paramlist} > ${1}/TOF-cut.txt
 
-filelist=`find $2|grep .root|grep .phys.hit.`
+filelist=`find $1|grep .root|grep .phys.hit.`
 echo ${filelist}
 for X in ${filelist}; do
 	echo "TOF cut : ${X}"
-	./TOF-cut-preselect -t root -f ${X} < ${2}/TOF-cut.txt
+	./TOF-cut-preselect -t root -f ${X} < ${1}/TOF-cut.txt
 done
 
 paramlist=""
-for X in `find $2|grep .root|grep .TOF.cut.`; do
+for X in `find $1|grep .root|grep .TOF.cut.`; do
 	paramlist=${paramlist}" "${X}
 done
 echo ${paramlist}
-./TOF-hists-after $1${paramlist} > ${2}/TOF-cut.txt
+./TOF-hists-after${paramlist} > ${1}/TOF-cut.txt
