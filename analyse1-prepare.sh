57d0d3d88b0cc5fc6f6e6c1392c3972407467f36
alexkernphysiker <kernphysiker@openmailbox.org>
2016-10-12 16:56:39 +0200

    script update

--- analyse1-prepare.sh
+++ analyse1-prepare.sh
@@ -7,4 +7,7 @@ for X in `find $1|grep .hld|grep -v .root`; do
 		echo "starting analysis"
 		./Preselect-Signals -t hld -f ${X}
 	fi
+	rm ${X%.*}.hld.times.root
+	rm ${X%.*}.hld.raw.root
+	rm ${X%.*}.hld.root
 done
