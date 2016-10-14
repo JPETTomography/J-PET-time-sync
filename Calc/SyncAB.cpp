be607dd42eb563f7e3b6798612bf343f5a660565
alexkernphysiker <kernphysiker@openmailbox.org>
2016-10-14 16:34:53 +0200

    fits: parameters changed

--- Calc/SyncAB.cpp
+++ Calc/SyncAB.cpp
@@ -37,7 +37,7 @@ namespace Sync{
 	});
 	fit.SetThreadCount(threads);
 	RANDOM r;
-	fit.Init(fit.Points()->size()*2,make_shared<GenerateByGauss>()
+	fit.Init(1000,make_shared<GenerateByGauss>()
 	    <<make_pair(total,total*30.0)
 	    <<make_pair((hist.left().X().min()+hist.right().X().max())/2.0,(hist.right().X().max()-hist.left().X().min())/2.0)
 	    <<make_pair(2.0,2.5)
