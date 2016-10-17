3bda3cab732dfaaa734e949989a90acc88a09de9
alexkernphysiker <kernphysiker@openmailbox.org>
2016-10-17 15:48:53 +0200

    fit parameters changed

--- Calc/SyncNeighbour.cpp
+++ Calc/SyncNeighbour.cpp
@@ -33,10 +33,10 @@ namespace Sync{
 	fit.Init(1000,make_shared<GenerateByGauss>()
 	    <<make_pair(total,total*30.0)
 	    <<make_pair((hist.left().X().min()+hist.right().X().max())/2.0,(hist.right().X().max()-hist.left().X().min())/2.0)
-	    <<make_pair(2.0,2.5)
+	    <<make_pair(0.5,0.5)
 	    <<make_pair(total,total*30.0)
 	    <<make_pair((hist.left().X().min()+hist.right().X().max())/2.0,(hist.right().X().max()-hist.left().X().min())/2.0)
-	    <<make_pair(2.0,2.5)
+	    <<make_pair(0.5,0.5)
 	,r);
 	cerr<<fit.ParamCount()<<" parameters"<<endl;
 	cerr<<fit.PopulationSize()<<" points"<<endl;
