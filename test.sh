e4cd68b966644b1d0e1c6ce97331385c0c2328c2
alexkernphysiker <kernphysiker@openmailbox.org>
2016-10-18 12:40:50 +0200

    test update

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@ echo ${filelist}
 for X in ${filelist}; do
 	echo "Test : ${X}"
 	./test1 -t root -f ${X}
-	paramlist=""
-	for X in `find $1|grep .root|grep .TestStat.`; do
-		paramlist=${paramlist}" "${X}
-	done
-	echo ${paramlist}
-	./test2${paramlist}
 done
+paramlist=""
+for X in `find $1|grep .root|grep .TestStat.`; do
+	paramlist=${paramlist}" "${X}
+done
+echo ${paramlist}
+./test2${paramlist}
