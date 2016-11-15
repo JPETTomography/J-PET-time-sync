428c1658dbd1d9a98d08988e69ccd461c2be11b5
alexkernphysiker <kernphysiker@openmailbox.org>
2016-11-15 12:24:46 +0100

    simplifying of AB-fit

--- Calc/SyncAB.cpp
+++ Calc/SyncAB.cpp
@@ -50,9 +50,7 @@ namespace Sync{
 	,r);
 	cerr<<fit.ParamCount()<<" parameters"<<endl;
 	cerr<<fit.PopulationSize()<<" points"<<endl;
-	ParamSet pExit{	1,	0.01,	0.001,	0.01,	0.001,	0.01,	0.001,	1,	0.0001	},
-		pDelta{	0.1,	0.01,	0.01,	0.001,	0.0001,	0.001,	0.0001,	0.1,	0.001	};
-	while((!fit.AbsoluteOptimalityExitCondition(0.0001))&&(!fit.ParametersDispersionExitCondition(pExit))){
+	while((!fit.AbsoluteOptimalityExitCondition(0.0001))&&(!fit.ParametersDispersionExitCondition(parEq(fit.ParamCount(),0.0001)))){
 	    fit.Iterate(r);
 	    cerr<<fit.iteration_count()<<" iterations; "
 	    <<fit.Optimality()<<"<chi^2<"
@@ -66,9 +64,8 @@ namespace Sync{
 	Plot<double>().Hist(hist,displayname).Line(totalfit,"Fit").Line(background,"Background")<<"set key on"<<"set xrange [-30:30]";
 	auto chi_sq_norm=fit.Optimality()/(fit.Points()->size()-fit.ParamCount());
 	cerr<<endl<<"done. chi^2/D="<<chi_sq_norm<<endl;
-	fit.SetUncertaintyCalcDeltas(pDelta);
-	for(const auto&p:fit.ParametersWithUncertainties())cerr<<p<<endl;
 	const auto&P=fit.Parameters();
+	for(const auto&p:P)cerr<<p<<endl;
 	return {.peak={P[1],P[2]},.chi_sq=chi_sq_norm};
     }
     
