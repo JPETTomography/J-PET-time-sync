9044de0291d04aa35abb7f5040943cb7d300136b
alexkernphysiker <kernphysiker@openmailbox.org>
2016-11-17 14:11:54 +0100

    scripts upgrade

--- RunSync.sh
+++ RunSync.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 ./analyse1-prepare.sh $2
-./analyse2-AB.sh $1 $2
-./analyse3-strips.sh $1 $2
+./analyse2-sync.sh $1 $2
--- analyse2-sync.sh
+++ analyse2-sync.sh
@@ -24,3 +24,20 @@ echo "Building TOT hists"
 ./TOT-hists -coincidence${paramlist} > /dev/null
 echo "Fitting A-B hists"
 ./Fit-AB $1${paramlist} > ${2}/AB-center.txt
+
+for X in `find $2|grep .root|grep .Hits.`; do
+	echo "Stage Strips : ${X}"
+	(cat ${2}/Deltas.txt  & cat ${2}/TOT-cut.txt & cat ${2}/AB-center.txt)|./Preselect-StripsSync -t root -f ${X}
+done
+paramlist=""
+for X in `find $2|grep .root|grep .Stat4Strips.`; do
+	paramlist=${paramlist}" "${X}
+done
+echo ${paramlist}
+./Fit-oposite $1${paramlist} > ${2}/Oposite-strips.txt
+./Fit-neighbour $1${paramlist} > ${2}/Neighbour-strips.txt
+
+(cat ${2}/Deltas.txt)|./Solve-equations $1 ${2}/AB-center.txt ${2}/Oposite-strips.txt ${2}/Neighbour-strips.txt > ${2}/Deltas.new.txt
+echo "replacing deltas"
+rm ${2}/Deltas.txt
+mv ${2}/Deltas.new.txt ${2}/Deltas.txt
