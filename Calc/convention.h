3032cead3a52a979583033fd2fff23979c7b5451
alexkernphysiker <kernphysiker@openmailbox.org>
2016-11-28 13:56:27 +0100

    convention.h formatted

--- Calc/convention.h
+++ Calc/convention.h
@@ -3,6 +3,20 @@
 #include <iostream>
 #include <math_h/sigma.h>
 #include <j-pet-framework-extension/PetDict.h>
+
+
+// Size of J-PET detector
+template<class DataType>
+inline const std::shared_ptr<JPetMap<DataType>> make_JPetMap(){
+  return std::shared_ptr<JPetMap<DataType>>(new JPetMap<DataType>({48,48,96}));
+}
+template<class DataType>
+inline const std::shared_ptr<JPetMap<DataType>> make_half_JPetMap(){
+  return std::shared_ptr<JPetMap<DataType>>(new JPetMap<DataType>({24,24,48}));
+}
+
+
+//Thresholds
 const size_t thresholds_count=4;
 struct TOT_cut{
   double A[thresholds_count],B[thresholds_count];
@@ -17,10 +31,9 @@ inline std::ostream&operator<<(std::ostream&str,const TOT_cut&item){
   for(size_t t=0;t<thresholds_count;t++)str<<item.B[t]<<" ";
   return str;
 }
-template<class DataType>
-inline const std::shared_ptr<JPetMap<DataType>> make_JPetMap(){
-  return std::shared_ptr<JPetMap<DataType>>(new JPetMap<DataType>({48,48,96}));
-}
+
+
+//Sync AB
 struct SyncAB_results{
   MathTemplates::value<double>peak;double chi_sq;
 };
@@ -30,6 +43,9 @@ inline std::istream&operator>>(std::istream&str,SyncAB_results&item){
 inline std::ostream&operator<<(std::ostream&str,const SyncAB_results&item){
   return str<<item.peak<<"\t"<<item.chi_sq;
 }
+
+
+//Hit-hit coincidences: oposite
 struct SyncOposite_results{
   MathTemplates::value<double>peak;double chi_sq;
 };
@@ -39,12 +55,9 @@ inline std::istream&operator>>(std::istream&str,SyncOposite_results&item){
 inline std::ostream&operator<<(std::ostream&str,const SyncOposite_results&item){
   return str<<item.peak<<"\t"<<item.chi_sq;
 }
-template<class DataType>
-inline const std::shared_ptr<JPetMap<DataType>> make_half_JPetMap(){
-  return std::shared_ptr<JPetMap<DataType>>(new JPetMap<DataType>({24,24,48}));
-}
 
-const std::vector<size_t> neighbour_delta_id{1,2,3};
+
+//Hit-hit coincidences for neighbour strips in layer
 struct SyncNeighbour_results{
   MathTemplates::value<double>left,right,assymetry;double chi_sq;
 };
@@ -54,6 +67,12 @@ inline std::istream&operator>>(std::istream&str,SyncNeighbour_results&item){
 inline std::ostream&operator<<(std::ostream&str,const SyncNeighbour_results&item){
   return str<<item.left<<"\t"<<item.right<<"\t"<<item.assymetry<<"\t"<<item.chi_sq;
 }
+const std::vector<size_t> neighbour_delta_id{1,2,3};
+
+
+
+
+// final results
 struct DeltaT_results{
   MathTemplates::value<double> A,B;
 };
