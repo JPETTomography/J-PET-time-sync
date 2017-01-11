81cadf7574c83d9156bded551681a1aec5fd03c3
alexkernphysiker <kernphysiker@openmailbox.org>
2017-01-11 16:26:14 +0100

    TOT conditions added

---
+++ Preselection/TOT-conditions.cpp
@@ -0,0 +1,16 @@
+// this file is distributed under 
+// MIT license
+#include <map>
+#include <JPetRawSignal/JPetRawSignal.h>
+#include <JPetLargeBarrelExtensions/PetDict.h>
+#include <JPetLargeBarrelExtensions/BarrelExtensions.h>
+#include "TOT-conditions.h"
+using namespace std;
+const bool TOT_conditions(const JPetHit&hit){
+    map<int,double> A = hit.getSignalA().getRecoSignal().getRawSignal().getTOTsVsThresholdNumber();
+    map<int,double> B = hit.getSignalB().getRecoSignal().getRawSignal().getTOTsVsThresholdNumber();
+    bool good=true;
+    for(size_t thr=1;good&&(thr<=4);thr++)
+	good&=(A[thr]>100.)&&(B[thr]>100.);
+    return good;
+}
