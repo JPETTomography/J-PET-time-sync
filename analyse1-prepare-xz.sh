7a28978adb1fa68ad3787aaaca5762d69bd64d75
alexkernphysiker <kernphysiker@openmailbox.org>
2017-01-31 14:28:00 +0100

    script for archives

---
+++ analyse1-prepare-xz.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for X in `find $1|grep .hld.xz|grep -v .root`; do
+	echo "File  ${X}"
+	if [ -e ${X%.*}.Hits.root ]; then
+		echo "has been analysed"
+	else
+		echo "starting analysis"
+ 		./Preselect-Hits -t zip -f ${X}
+	fi
+	rm ${X%.*}.hld.times.root
+	rm ${X%.*}.hld.raw.root
+	rm ${X%.*}.hld.root
+	rm ${X%.*}.hld
+done
+
