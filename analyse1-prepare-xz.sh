efc128814b5940ffe65964a29152a8b547478337
rundel <alexrndl@gmail.com>
2017-01-31 15:22:42 +0100

    script update

--- analyse1-prepare-xz.sh
+++ analyse1-prepare-xz.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-for X in `find $1|grep .xz|grep -v .root`; do
+for X in `find $1|grep .hld.xz|grep -v .root`; do
 	echo "File  ${X}"
 	if [ -e ${X%.*}.Hits.root ]; then
 		echo "has been analysed"
 	else
 		echo "starting analysis"
- 		./Preselect-Hits -t zip -f ${X}
+		tar xf ${X}
+ 		./Preselect-Hits -t hld -f ${X%.*}
 	fi
 	rm ${X%.*}.hld.times.root
 	rm ${X%.*}.hld.raw.root
