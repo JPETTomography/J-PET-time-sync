989ffb0eba57f9ed98fb2b1474e7fdd30614d56a
alexkernphysiker <kernphysiker@openmailbox.org>
2017-02-05 00:20:57 +0100

    first stage of analysis can run in multithread

--- analyse1-prepare-xz.sh
+++ analyse1-prepare-xz.sh
@@ -4,13 +4,17 @@ for X in `find $1|grep .hld.xz|grep -v .root`; do
 	if [ -e ${X%.*}.Hits.root ]; then
 		echo "has been analysed"
 	else
+	    if [ -e ${X%.*} ]; then
+		echo "is being analysed"
+	    else
 		echo "starting analysis"
 		unxz ${X}
- 		./Preselect-Hits -t hld -f ${X%.*}
+		./Preselect-Hits -t hld -f ${X%.*}
+		rm ${X%.*}.times.root
+		rm ${X%.*}.raw.root
+		rm ${X%.*}.root
+		rm ${X%.*}
+	    fi
 	fi
-	rm ${X%.*}.times.root
-	rm ${X%.*}.raw.root
-	rm ${X%.*}.root
-	rm ${X%.*}
 done
 
--- analyse1-prepare.sh
+++ analyse1-prepare.sh
@@ -4,10 +4,14 @@ for X in `find $1|grep .hld|grep -v .root`; do
 	if [ -e ${X%.*}.Hits.root ]; then
 		echo "has been analysed"
 	else
+	    if [ -e ${X%.*}.root ]; then
+		echo "is being analysed"
+	    else
 		echo "starting analysis"
- 		./Preselect-Hits -t hld -f ${X}
+		./Preselect-Hits -t hld -f ${X}
+		rm ${X}.times.root
+		rm ${X}.raw.root
+		rm ${X}.root
+	    fi
 	fi
-	rm ${X}.times.root
-	rm ${X}.raw.root
-	rm ${X}.root
 done
